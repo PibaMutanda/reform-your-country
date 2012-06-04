b28c9cdd847bf2db4c934c30f8e5b353aed62703
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-04 12:42:36 +0000

    throw usernotfoundexception

--- src/blackbelt/service/UserService.java
+++ src/blackbelt/service/UserService.java
@@ -2,6 +2,7 @@ package blackbelt.service;
 
 import java.util.List;
 
+import blackbelt.exceptions.UserNotFoundException;
 import blackbelt.model.User;
 
 public interface UserService {
@@ -11,8 +12,8 @@ public interface UserService {
 	void changeUserName(User user, String newFirstName, String newLastName);
 
 	List<String> assertNicknamesExists(List<String> nicknames,
-			boolean resultSorted);
+			boolean resultSorted) throws UserNotFoundException;
 
-	String ensureNicknameUniqueness(String nickNameToTest);
+	String ensureNicknameUniqueness(String nickNameToTest) throws UserNotFoundException;
 
 }
