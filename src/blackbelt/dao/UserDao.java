456f9b383a89ba5b45b27760eea9afb0bf7e1c31
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-04 12:39:46 +0000

    the code works now and throw usernotfoundexception

--- src/blackbelt/dao/UserDao.java
+++ src/blackbelt/dao/UserDao.java
@@ -4,25 +4,32 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import blackbelt.exceptions.UserNotFoundException;
 import blackbelt.model.User;
 
 public class UserDao {
 
-	private static List<User> listeUser = new ArrayList<>();
+	private static List<User> listeUser = new ArrayList<User>();
 
-	// TODO maxime delete static modifier when using JPA
-	public static void save(User user) {
-		listeUser.add(user);
+	public static List<User> getListeUser() {
+		return listeUser;
 	}
 
 	// TODO maxime delete static modifier when using JPA
-	public static User get(Long id) {
+	public static void save(User user) {
+		System.out.println("dao je save " + user);
+		listeUser.add(user);
+		System.out.println(listeUser.size());
+	}
+
+	// TODO maxime delete static modifier when using JPA
+	public static User get(Long id) throws UserNotFoundException {
 
 		for (User user : listeUser) {
-			return id == user.getId() ? user : null;
+			if (id.equals(user.getId()))
+				return user;
 		}
-
-		return null;
+		throw new UserNotFoundException(id.toString());
 	}
 
 	// use createTestUser() user in main instead
@@ -39,21 +46,23 @@ public class UserDao {
 	}
 
 	// TODO maxime delete static modifier when using JPA
-	public static User getUserByEmail(String identifier) {
-		for (User user : listeUser) {
-
-			return identifier.equals(user.getMail()) ? user : null;
-		}
-		return null;
+	public static User getUserByEmail(String identifier) throws UserNotFoundException {
+			for (User user : listeUser) {
+				if (identifier.equals(user.getMail()))
+					return user;
+			}
+			throw new UserNotFoundException(identifier);
 	}
 
 	// TODO maxime delete static modifier when using JPA
-	public static User getUserByNickName(String identifier) {
+	public static User getUserByNickName(String identifier)
+			throws UserNotFoundException {
 		for (User user : listeUser) {
-			return identifier.equals(user.getNickName()) ? user : null;
+			if (identifier.equals(user.getNickName()))
+				return user;
 		}
-
-		return null;
+		throw new UserNotFoundException(identifier,
+				"le user avnt le nickname précédent n'a pu être trouvé");
 	}
 
 }
