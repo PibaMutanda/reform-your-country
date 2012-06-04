3fe0930a6b683109ebd951b931424a5080de165a
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-04 13:40:54 +0000

    succesfully tested login

--- trunk/src/MainTestUser.java
+++ trunk/src/MainTestUser.java
@@ -3,10 +3,15 @@ import java.util.Iterator;
 import java.util.List;
 
 import blackbelt.dao.UserDao;
+import blackbelt.exceptions.InvalidPasswordException;
+import blackbelt.exceptions.UserLockedException;
 import blackbelt.exceptions.UserNotFoundException;
+import blackbelt.exceptions.UserNotValidatedException;
 import blackbelt.impl.UserServiceImpl;
 import blackbelt.model.User;
 import blackbelt.model.User.Gender;
+import blackbelt.service.LoginService;
+import blackbelt.service.LoginService.WaitDelayNotReachedException;
 import blackbelt.service.UserService;
 import blackbelt.util.SecurityUtils;
 
@@ -19,8 +24,8 @@ public class MainTestUser {
 
 		UserServiceImpl.registerUser(true, "maxime", "Sauvage", Gender.MR,
 				"max", "max", "max@home.be");
-		UserServiceImpl.registerUser(true, "piba", "mutunba", Gender.MR, "max",
-				"max", "piba@home.be");
+		UserServiceImpl.registerUser(true, "piba", "mutunba", Gender.MR,
+				"piba", "max", "piba@home.be");
 
 		try {
 			System.out.println("recherche par le dao"
@@ -29,20 +34,30 @@ public class MainTestUser {
 					+ UserDao.getUserByEmail("max@home.be"));
 			System.out.println("recherche par le dao"
 					+ UserDao.getUserByEmail("piba@home.be"));
-		} catch (UserNotFoundException e) {
+			User loggedIn = testLogin("max", "max");
+			if (loggedIn != null)
+				System.out.println("succesfull logged in as "
+						+ loggedIn.getNickName());
+			User loggedIn2 = testLogin("piba@home.be", "max");
+			if (loggedIn2 != null)
+				System.out.println("succesfull logged in as "
+						+ loggedIn2.getNickName());
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+	}
+
+	public static void consoleFormRegistration() {
 
 	}
 
-	 public static void consoleFormRegistration() {
-	
-	 }
-
-	 public static boolean testLogin() {
-	 return false;
-	
-	 }
+	public static User testLogin(String identifier, String clearPassword)
+			throws Exception {
+		LoginService login = new LoginService();
+		return login.login(identifier, clearPassword, false);
+		// return login.loginEncrypted(identifier,
+		// "477bc098b8f2606137c290f9344dcee8", false);
+	}
 }
