de91207ec35b97e87e621cf989ccc797dcfec6ef
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-04 10:09:55 +0000

    user UserService instead of direct call Dao

--- trunk/src/MainTestUser.java
+++ trunk/src/MainTestUser.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import blackbelt.dao.UserDao;
+import blackbelt.impl.UserServiceImpl;
 import blackbelt.model.User;
 import blackbelt.model.User.Gender;
+import blackbelt.service.UserService;
 
 public class MainTestUser {
 
@@ -13,10 +16,17 @@ public class MainTestUser {
 	public static void main(String[] args) {
 
 		User test = createTestUser("maxime", "Sauvage", Gender.MR, "max", "max");
-		UserDao.save(test);
-		System.out.println(UserDao.getUserByNickName("max"));
-		
-		
+		UserServiceImpl service = new UserServiceImpl();
+		service.registerUser(test, true);
+		try {
+			service.deleteUser(test, true, true);
+		} catch (UnsupportedOperationException e) {
+			System.out
+					.println("on ne peut pas encore supprimer de user , c'est balot...");
+		}
+		System.out.println("recherche par le dao"
+				+ UserDao.getUserByNickName("max"));
+
 		UserDao userdao = new UserDao();
 		ArrayList myListe = (ArrayList<User>) userdao.createListeUser();
 		Iterator it = myListe.iterator();
@@ -36,5 +46,9 @@ public class MainTestUser {
 		user.setNickName(nickname);
 		user.setPassword(password);
 		return user;
+	}
+
+	public static void consoleFormRegistration() {
+
 	}
 }
