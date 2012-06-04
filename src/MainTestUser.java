7368e4e085c818214e348b0e2227d0b3cfd78dd4
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-04 09:46:19 +0000

    create method who create and return a user and test it in main.

--- src/MainTestUser.java
+++ src/MainTestUser.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 
 import blackbelt.dao.UserDao;
 import blackbelt.model.User;
-
+import blackbelt.model.User.Gender;
 
 public class MainTestUser {
 
@@ -12,15 +12,25 @@ public class MainTestUser {
 	 */
 	public static void main(String[] args) {
 
-	UserDao userdao=new UserDao();
-	ArrayList myListe=(ArrayList<User>)userdao.createListeUser();          
-	Iterator it=myListe.iterator();
-	while(it.hasNext()){
-		User user=(User) it.next();
-		System.out.println(user.toString()+"\n");
+		User test = createTestUser("maxime", "Sauvage", Gender.MR, "max","max");
+		UserDao userdao = new UserDao();
+		ArrayList myListe = (ArrayList<User>) userdao.createListeUser();
+		Iterator it = myListe.iterator();
+		while (it.hasNext()) {
+			User user = (User) it.next();
+			System.out.println(user.toString() + "\n");
+		}
+
 	}
 
-	
+	public static User createTestUser(String firstName, String lastName,
+			Gender gender, String nickname, String password) {
+		User user = new User();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setGender(gender);
+		user.setNickName(nickname);
+		user.setPassword(password);
+		return user;
 	}
-
 }
