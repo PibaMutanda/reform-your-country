91301af694004943fd6ea4ca86b24167a39c172f
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-04 07:57:29 +0000

    change modifier to static

--- trunk/src/blackbelt/dao/UserDao.java
+++ trunk/src/blackbelt/dao/UserDao.java
@@ -4,63 +4,57 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-
 import blackbelt.model.User;
 
-
 public class UserDao {
-	
-	private List<User> listeUser=new ArrayList<>();
-	
-	
-	public void save(User user) {
-		// TODO Auto-generated method stub
-		listeUser.add(user);		
-	}
 
-	public User get(Long id) {
-		// TODO Auto-generated method stub
-		User user=new User();
-		
-		Iterator it=listeUser.iterator();
-		
-		while(it.hasNext()){
-			user=(User)it.next();
-			return id==user.getId() ? user:null;
-		}
-	 
-	 return null;
-	}
-	
-	public List<User> createListeUser() {
+	private static List<User> listeUser = new ArrayList<>();
 
-		   User user=new User();
-		        user.setFirstName("Helios");
-		        user.setLastName("Toto");
-		        
-		        listeUser.add(user);
-		
-		return listeUser;
-	}
-
-	public User getUserByEmail(String identifier) {
-		// TODO Auto-generated method stub
-		for (User user : listeUser) {
-			   
-			return identifier.equals(user.getMail())?user:null;
-		}
-		return null;
-	}
-
-	public  User getUserByNickName(String identifier) {
-		// TODO Auto-generated method stub
-		for (User user: listeUser) {
-			 return identifier.equals(user.getNickName())?user:null;
-		}
-		
-		
-		return null;
+	// TODO maxime delete static modifier when using JPA
+	public static void save(User user) {
+		listeUser.add(user);
 	}	
 	
+	// TODO maxime delete static modifier when using JPA
+	public static User get(Long id) {
+		User user = new User();
+
+		Iterator it = listeUser.iterator();
+
+		while (it.hasNext()) {
+			user = (User) it.next();
+			return id == user.getId() ? user : null;
+		}
+
+		return null;
+	}
+
+	public List<User> createListeUser() {
+
+		User user = new User();
+		user.setFirstName("Helios");
+		user.setLastName("Toto");
+
+		listeUser.add(user);
+
+		return listeUser;
+	}
+	// TODO maxime delete static modifier when using JPA
+	public static User getUserByEmail(String identifier) {
+		for (User user : listeUser) {
+
+			return identifier.equals(user.getMail()) ? user : null;
+		}
+		return null;
+	}
+
+	// TODO maxime delete static modifier when using JPA
+	public static User getUserByNickName(String identifier) {
+		for (User user : listeUser) {
+			return identifier.equals(user.getNickName()) ? user : null;
+		}
+
+		return null;
+	}
 
 }
