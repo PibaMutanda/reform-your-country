4966663f71489b2aba2b9bdae55444f4d966dbe9
jeromepanaioli@gmail.com <jeromepanaioli@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-06 13:37:33 +0000

    Main du test de la page de couverture

---
+++ ReformYourCountry/src/packagePdf/MainCoverPage.java
@@ -0,0 +1,26 @@
+package packagePdf;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class MainCoverPage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		File filetest = new File("S:\\coverTest.pdf");
+		try {
+			FileOutputStream fos =new FileOutputStream(filetest);
+			TestCoverPage cPdf = new TestCoverPage();
+			cPdf.generatePdf(fos, true, false);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
