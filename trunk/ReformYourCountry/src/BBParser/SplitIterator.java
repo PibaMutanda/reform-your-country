f3a3ba8852ce06068890455ea05c4ac1344a80b4
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-06 12:11:30 +0000

    switch to java convention formatter

--- trunk/ReformYourCountry/src/BBParser/SplitIterator.java
+++ trunk/ReformYourCountry/src/BBParser/SplitIterator.java
@@ -5,35 +5,40 @@ import java.io.Reader;
 import java.util.Iterator;
 
 /**
- * Created by IntelliJ IDEA. User: Alexey Date: 12.06.11 Time: 14:16 To change this template use File | Settings | File
- * Templates.
+ * Created by IntelliJ IDEA. User: Alexey Date: 12.06.11 Time: 14:16 To change
+ * this template use File | Settings | File Templates.
  */
-class SplitIterator implements Iterator<Part> {
-    private StringBuilder buf = new StringBuilder();
+class SplitIterator implements Iterator<Part>
+{
+    private StringBuilder buf       = new StringBuilder();
 
-    private boolean braceOpen = false;
-    private char openQuote = 0;
+    private boolean       braceOpen = false;
+    private char          openQuote = 0;
 
-    private final Reader source;
+    private final Reader  source;
 
-    private Part nextPart;
-    
-    private boolean error=false;
+    private Part          nextPart;
 
-    public SplitIterator(Reader source) {
+    private boolean       error     = false;
+
+    public SplitIterator(Reader source)
+    {
         this.source = source;
     }
-    
-    public boolean getError(){
-    	return this.error;
+
+    public boolean getError()
+    {
+        return this.error;
     }
 
-
     @Override
-    public boolean hasNext() {
-        try {
+    public boolean hasNext()
+    {
+        try
+        {
             nextPart = readNextPart();
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             return false;
         }
 
@@ -41,64 +46,82 @@ class SplitIterator implements Iterator<Part> {
     }
 
     @Override
-    public Part next() {
+    public Part next()
+    {
         return nextPart;
     }
 
     @Override
-    public void remove() {
+    public void remove()
+    {
     }
 
-    private Part readNextPart() throws IOException {
+    private Part readNextPart() throws IOException
+    {
         Part result = null;
 
         int v;
-        while ((v = source.read()) != -1) {
+        while ((v = source.read()) != -1)
+        {
             char c = (char) v;
 
-            if (c == '[') {
-                if (!braceOpen) {
-                    if (buf.length() > 0) {
+            if (c == '[')
+            {
+                if (!braceOpen)
+                {
+                    if (buf.length() > 0)
+                    {
                         result = new Part(buf.toString());
                         buf.setLength(0);
                     }
                     braceOpen = true;
-                }else{
-                	error=true;
+                } else
+                {
+                    error = true;
                     result = new Part(buf.toString());
                     return result;
                 }
                 buf.append(c);
-            } else if ((c == '"' || c == '\'') && braceOpen) {
-                if (openQuote == 0) {
+            } else if ((c == '"' || c == '\'') && braceOpen)
+            {
+                if (openQuote == 0)
+                {
                     openQuote = c;
-                } else if (openQuote == c) {
+                } else if (openQuote == c)
+                {
                     openQuote = 0;
                 }
                 buf.append(c);
-            } else if (c == ']' && braceOpen) {
+            } else if (c == ']' && braceOpen)
+            {
                 buf.append(c);
-                if (buf.length() > 0) {
+                if (buf.length() > 0)
+                {
                     result = new Part(buf.toString());
                     buf.setLength(0);
                 }
                 braceOpen = false;
-            } else if (c == '\n' && !braceOpen) {
+            } else if (c == '\n' && !braceOpen)
+            {
                 buf.append(c);
-                if (buf.length() > 0) {
+                if (buf.length() > 0)
+                {
                     result = new Part(buf.toString());
                     buf.setLength(0);
                 }
-            } else {
+            } else
+            {
                 buf.append(c);
             }
 
-            if (result != null) {
+            if (result != null)
+            {
                 return result;
             }
         }
 
-        if (buf.length() > 0) {
+        if (buf.length() > 0)
+        {
             result = new Part(buf.toString());
             buf.setLength(0);
         }
