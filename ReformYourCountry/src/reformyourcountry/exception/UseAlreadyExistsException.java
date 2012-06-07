557e6999b1d953ab811e4f97f93d3826605d45c5
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-07 14:04:02 +0000

    create useralreadyexistsexception

---
+++ ReformYourCountry/src/reformyourcountry/exception/UseAlreadyExistsException.java
@@ -0,0 +1,32 @@
+package reformyourcountry.exception;
+
+
+public class UseAlreadyExistsException extends Exception {
+    private static final long serialVersionUID = 5049031236161497138L;
+
+    public enum identifierType {
+	MAIL("email"), USERNAME("username");
+	private String type;
+
+	identifierType(String type) {
+	    this.type = type;
+	}
+	
+	public String toString()
+	{
+	    return type;
+	}
+    }
+    
+    private identifierType type;
+    private String identifier;
+    
+    public UseAlreadyExistsException(identifierType type, String identifier) {
+	super("user already exist for the "+type.toString()+" identifer : "+identifier);
+	this.type = type;
+	this.identifier = identifier;
+    }
+    
+    
+
+}
