7344a040f0f05af9e3ee4f78e146e5384911fb40
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-07 10:09:54 +0000

    fix package name

---
+++ trunk/ReformYourCountry/src/reformyourcountry/test/TestCommit.java
@@ -0,0 +1,13 @@
+package reformyourcountry.test;
+
+public class TestCommit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.println("test de svn nouveau");
+
+	}
+
+}
