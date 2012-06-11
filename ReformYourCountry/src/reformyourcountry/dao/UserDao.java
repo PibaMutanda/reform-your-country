04f5ea91770c31e50b520ab0cc32e2b1672c96bf
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-11 09:55:03 +0000

    remove user method added. change db to set because with arraylis , save create double user in db.

--- ReformYourCountry/src/reformyourcountry/dao/UserDao.java
+++ ReformYourCountry/src/reformyourcountry/dao/UserDao.java
@@ -1,54 +1,72 @@
 package reformyourcountry.dao;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import reformyourcountry.model.User;
-
 import blackbelt.exceptions.UserNotFoundException;
 
 public class UserDao {
 
-	private  List<User> listeUser = new ArrayList<User>();
+    private  static Set<User> listeUser = new HashSet<User>();
 
-	public  List<User> getListeUser() {
-		return listeUser;
+    @SuppressWarnings("unchecked")
+    public  List<User> getListeUser() {
+	return (List<User>) listeUser;
+    }
+
+    // TODO maxime delete static modifier when using JPA
+    public void save(User user) {
+	System.out.println("dao je save " + user);
+	if(!listeUser.add(user))
+	{
+	    try {
+		remove(user.getUserName());
+	    } catch (UserNotFoundException e) {
+		e.printStackTrace();
+	    }
+	    listeUser.add(user);
 	}
-
-	// TODO maxime delete static modifier when using JPA
-	public  void save(User user) {
-		System.out.println("dao je save " + user);
-		listeUser.add(user);
-		System.out.println(listeUser.size());
+    }
+    //needed for JUnitTest data not annoying official data
+    public void remove(String identifier) throws UserNotFoundException
+    {
+	for (User user : listeUser) {
+	    if (identifier.equals(user.getUserName())) {
+		System.out.println("userDao je remove : "+user.getUserName());
+		listeUser.remove(user);
+		break;
+	    }
 	}
+    }
 
-	// TODO maxime delete static modifier when using JPA
-	public  User get(Long id) {
+    // TODO maxime delete static modifier when using JPA
+    public  User get(Long id) {
 
-		for (User user : listeUser) {
-			if (id.equals(user.getId()))
-				return user;
-		}
-		return null;
+	for (User user : listeUser) {
+	    if (id.equals(user.getId()))
+		return user;
 	}
+	return null;
+    }
 
-	// TODO maxime delete static modifier when using JPA
-	public  User getUserByEmail(String identifier)  {
-			for (User user : listeUser) {
-				if (identifier.equals(user.getMail()))
-					return user;
-			}
-			return null;
+    // TODO maxime delete static modifier when using JPA
+    public  User getUserByEmail(String identifier)  {
+	for (User user : listeUser) {
+	    if (identifier.equals(user.getMail()))
+		return user;
 	}
+	return null;
+    }
 
-	// TODO maxime delete static modifier when using JPA
-	public  User getUserByUserName(String identifier) {
-		for (User user : listeUser) {
-			if (identifier.equals(user.getUserName()))
-				return user;
-		}
-		return null;
+    // TODO maxime delete static modifier when using JPA
+    public  User getUserByUserName(String identifier) {
+	for (User user : listeUser) {
+	    if (identifier.equals(user.getUserName()))
+		return user;
 	}
+	return null;
+    }
 
 }
