8d2d52a3d9c5824432aa79bd5cce944baba2b543
jeromepanaioli@gmail.com <jeromepanaioli@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-13 13:11:46 +0000

    repository for testing the security

---
+++ trunk/ReformYourCountry/src/reformyourcountry/security/MainSecurity.java
@@ -0,0 +1,13 @@
+package reformyourcountry.security;
+import blackbelt.security.*;
+public class MainSecurity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
