210eed513aab476bd8963c22782f4ceb92ea5f14
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-06-14 17:22:25 +0000

    fix commiter addict

--- trunk/ReformYourCountry/src/blackbelt/parser/deprecated/Main.java
+++ trunk/ReformYourCountry/src/blackbelt/parser/deprecated/Main.java
@@ -4,13 +4,8 @@ package blackbelt.parser.deprecated;
 public class Main {
 	public static void main(String[] args){
 		MyTagHandler tg = new MyTagHandler();
-<<<<<<< .mine
-		BlackBeltTagParser parser = new BlackBeltTagParser(tg, "[quote]test [code]double balise[code]"+
-																"[/quote]blabla[...] [code]ceci [quote]est[/quote] du code[/code]");
-=======
 		BlackBeltTagParser parser = new BlackBeltTagParser(tg, "[/quote]test [/code]double balise[/code]"+
 																"[/quote]blabla[...] [code]ceci [quote]est[/quote] du code[/code]");
->>>>>>> .r70
 		
 		System.out.println(tg.addParagraph(tg.getOutputString(parser.parse())));
 		
