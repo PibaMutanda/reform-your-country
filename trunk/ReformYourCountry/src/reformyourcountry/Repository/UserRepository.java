e5be0fc4abb06909ab13f243f28f89e75f4f93ee
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-07-02 10:00:50 +0000

    use createquery instead of namedqueries

--- trunk/ReformYourCountry/src/reformyourcountry/Repository/UserRepository.java
+++ trunk/ReformYourCountry/src/reformyourcountry/Repository/UserRepository.java
@@ -2,12 +2,7 @@ package reformyourcountry.Repository;
 
 import java.util.List;
 
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceUnit;
-
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import reformyourcountry.model.User;
 
@@ -16,14 +11,14 @@ import reformyourcountry.model.User;
 public class UserRepository extends BaseRepository<User>{
 
     public  User getUserByEmail(String identifier)  {
-        List<User> results= em.createNamedQuery("findUserByEmail").setParameter("mail", identifier).getResultList();
+        List<User> results= em.createQuery("select u from User u where u.mail =:mail").setParameter("mail", identifier).getResultList();
         if(results.isEmpty())
             return null;
         return results.get(0);
     }
 
     public  User getUserByUserName(String identifier) {
-        List<User> results= em.createNamedQuery("findUserByUserName").setParameter("userName", identifier).getResultList();
+        List<User> results= em.createQuery("select u from User u where u.userName =:userName").setParameter("userName", identifier).getResultList();
         if(results.isEmpty())
             return null;
         return results.get(0);
