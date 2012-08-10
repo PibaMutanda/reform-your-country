19b85171b15306eddf4935bd835acf7bc618e61b
Thom.VanRoy@gmail.com <Thom.VanRoy@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-08-10 13:45:15 +0000

    Springifying project

---
+++ ReformYourCountry/src/reformyourcountry/exception/UserAlreadyExistsException.java
@@ -0,0 +1,32 @@
+package reformyourcountry.exception;
+
+
+public class UserAlreadyExistsException extends Exception {
+    private static final long serialVersionUID = 5049031236161497138L;
+
+    public enum identifierType {
+	MAIL("email"), USERNAME("username");
+	private String type;
+
+	identifierType(String type) {
+	    this.type = type;
+	}
+	
+	public String toString()
+	{
+	    return type;
+	}
+    }
+    
+    private identifierType type;
+    private String identifier;
+    
+    public UserAlreadyExistsException(identifierType type, String identifier) {
+	super("user already exist for the "+type.toString()+" identifer : "+identifier);
+	this.type = type;
+	this.identifier = identifier;
+    }
+    
+    
+
+}
