4d2fae51d068d8e8c026e1827a01d1406ed3881d
Thom.VanRoy@gmail.com <Thom.VanRoy@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-08-10 13:45:15 +0000

    Springifying project

---
+++ trunk/ReformYourCountry/src/reformyourcountry/exception/UnauthorizedAccessException.java
@@ -0,0 +1,24 @@
+package reformyourcountry.exception;
+
+import reformyourcountry.security.Privilege;
+
+@SuppressWarnings("serial")
+public class UnauthorizedAccessException extends com.sun.servicetag.UnauthorizedAccessException{
+                   
+       public UnauthorizedAccessException() {
+                super();
+             }
+            
+       public UnauthorizedAccessException(String message) {
+                super();
+                this.message = message;
+            }
+       
+       public UnauthorizedAccessException(Privilege privilege) {
+                super();
+                this.privilege = privilege;
+            }
+            
+ private Privilege privilege;
+ private String message;   
+}
