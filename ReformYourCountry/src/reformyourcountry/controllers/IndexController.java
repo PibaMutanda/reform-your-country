48a3641557ae340ef2704525749029a44df84b8b
Thom.VanRoy@gmail.com <Thom.VanRoy@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-08-10 12:10:05 +0000

    Springifying project

--- ReformYourCountry/src/reformyourcountry/controllers/IndexController.java
+++ ReformYourCountry/src/reformyourcountry/controllers/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+	
 	@RequestMapping(value={"/index","/INDEX","/Index","/"})
 	public String index(){
 		return "index";
