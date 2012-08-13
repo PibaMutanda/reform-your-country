4049ab7f80f32a0342e4ccc81600a8ea26921b84
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-08-13 12:21:47 +0000

    clean import and redirect to home page

--- ReformYourCountry/src/reformyourcountry/controller/LoginController.java
+++ ReformYourCountry/src/reformyourcountry/controller/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import reformyourcountry.exception.InvalidPasswordException;
 import reformyourcountry.exception.UserLockedException;
@@ -47,6 +46,6 @@ public class LoginController {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return "index";
+        return "home";
     }
 }
