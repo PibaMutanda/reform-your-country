bb084361ff121414b92369c61b07e6f6d1422c04
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-08-31 14:24:44 +0000

    login in DEV without pswd

--- trunk/ReformYourCountry/WebContent/WEB-INF/views/login.jsp
+++ trunk/ReformYourCountry/WebContent/WEB-INF/views/login.jsp
@@ -1,8 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <%@ taglib uri="http://www.springframework.org/tags/form"  prefix="form" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>   
-<%@ page import="reformyourcountry.web.ContextUtil" %>
+<%@ taglib uri='/WEB-INF/tags/ryc.tld' prefix='ryc'%>
 <html>
 <head>
 <title>Login</title>
@@ -11,9 +10,9 @@
      ${error}
       <form action="loginsubmit" method="post">
              <label for="identifier">Pseudo/Adresse e-mail </label><input type="text" name="identifier" required="required"><br/>
-             <c:if test="${reformyourcountry.web.ContextUtil.getEnvironment() != DEV}">
+             <ryc:conditionDisplay environment="PROD" >
                 <label for="password">Votre mot de passe </label> <input type="password" name="password" required="required"><br/>
-             </c:if>
+             </ryc:conditionDisplay>
              J'ai oublié  mon <a href="">mot de passe</a><br/>
              <input type="checkbox" name="keepLoggedIn"/><label for="keepLoggedIn">Je souhaite rester connecté</label><br/> 
              <input type="submit"  value="Connexion"/>                   
--- trunk/ReformYourCountry/src/reformyourcountry/tag/ConditionDisplayTag.java
+++ trunk/ReformYourCountry/src/reformyourcountry/tag/ConditionDisplayTag.java
@@ -6,35 +6,46 @@ import javax.servlet.jsp.JspException;
 
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
+import reformyourcountry.misc.CurrentEnvironment.Environment;
 import reformyourcountry.security.Privilege;
 import reformyourcountry.security.SecurityContext;
+import reformyourcountry.web.ContextUtil;
 
 /** Prevents a JSP from executing if the user does not have a specific privilege */
 public class ConditionDisplayTag extends SimpleTagSupport {
     
     
     Privilege privilege;
-    
-    
+    Environment environment;
+
     public Privilege getPrivilege() {
         return privilege;
     }
 
+    public Environment getEnvironment() {
+        return environment;
+    }
 
     public void setPrivilege(Privilege privilege) {
         this.privilege = privilege;
     }
 
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
 
     @Override
     public void doTag() throws JspException, IOException {
-    	if(SecurityContext.isUserHasPrivilege(privilege)){
+        //FIXME what if we want to test the two values? -- maxime 31/08/12
+        if(environment != null && ContextUtil.getEnvironment().equals(environment)){
             
-    		   
             getJspBody().invoke(null);
+        }else if(privilege != null && SecurityContext.isUserHasPrivilege(privilege)){
             
-            
-            }
+            getJspBody().invoke(null);
+        }
+
     }
 
 }
