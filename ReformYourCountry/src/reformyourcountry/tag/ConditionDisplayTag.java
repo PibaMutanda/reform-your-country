fe43c06dbac5d9a7bb0b1d88e7d0607446e1fb19
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-03 11:06:20 +0000

    remove environment

--- ReformYourCountry/src/reformyourcountry/tag/ConditionDisplayTag.java
+++ ReformYourCountry/src/reformyourcountry/tag/ConditionDisplayTag.java
@@ -3,46 +3,31 @@ package reformyourcountry.tag;
 import java.io.IOException;
 
 import javax.servlet.jsp.JspException;
-
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
-import reformyourcountry.misc.CurrentEnvironment.Environment;
 import reformyourcountry.security.Privilege;
 import reformyourcountry.security.SecurityContext;
-import reformyourcountry.web.ContextUtil;
 
-/** Only display the body if the user has a specific privilege */
+/** Prevents a JSP from executing if the user does not have a specific privilege */
 public class ConditionDisplayTag extends SimpleTagSupport {
     
     
     Privilege privilege;
-    Environment environment;
 
     public Privilege getPrivilege() {
         return privilege;
     }
 
-    public Environment getEnvironment() {
-        return environment;
-    }
 
     public void setPrivilege(Privilege privilege) {
         this.privilege = privilege;
     }
 
-    public void setEnvironment(Environment environment) {
-        this.environment = environment;
-    }
 
 
     @Override
     public void doTag() throws JspException, IOException {
-        //FIXME what if we want to test the two values? -- maxime 31/08/12
-        if(environment != null && ContextUtil.getEnvironment().equals(environment)){
-            
-            getJspBody().invoke(null);
-        }else if(privilege != null && SecurityContext.isUserHasPrivilege(privilege)){
-            
+        if(privilege != null && SecurityContext.isUserHasPrivilege(privilege)){
             getJspBody().invoke(null);
         }
 
