d75610ffbfb99c3e820b2f36dd014b7d9f8b6e55
lionel.timmerman@gmail.com <lionel.timmerman@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-07 08:44:06 +0000

    new folders added for user image

--- ReformYourCountry/src/reformyourcountry/util/FileUtil.java
+++ ReformYourCountry/src/reformyourcountry/util/FileUtil.java
@@ -26,6 +26,11 @@ public abstract class FileUtil {
 	final static public String BOOK_ORIGINAL_SUB_FOLDER = "/original";
 	final static public String BOOK_RESIZED_SUB_FOLDER = "/resized";
 	final static public String ARTICLE_SUB_FOLDER = "/article";
+	final static public String USER_SUB_FOLDER = "/user";
+	final static public String USER_ORIGINAL_SUB_FOLDER = "/original";
+	final static public String USER_RESIZED_SUB_FOLDER = "/resized";
+	//final static public String USER_RESIZED_LARGE_SUB_FOLDER = "/large";
+	//final static public String USER_RESIZED_SMALL_SUB_FOLDER = "/small";
 
     static Log log = LogFactory.getLog(FileUtil.class);
 	
