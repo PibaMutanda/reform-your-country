4139683fbca102bd2a9e6d7f8b9fb066f642c40c
bouda.jamal@gmail.com <bouda.jamal@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-07 07:53:44 +0000

    action display

---
+++ ReformYourCountry/src/reformyourcountry/repository/ActionRepository.java
@@ -0,0 +1,15 @@
+package reformyourcountry.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import reformyourcountry.model.Action;
+
+@Repository
+@SuppressWarnings("unchecked")
+public class ActionRepository extends BaseRepository<Action> {
+
+    public List<Action> findAll(){
+        return    em.createQuery("select a from Action a").getResultList();
+    }
+}
