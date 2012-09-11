94eaa09e1c2c0823b9c9e6ef927095a8f42acd98
johnnyshamoon@gmail.com <johnnyshamoon@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-11 08:41:43 +0000

    Action List

--- trunk/ReformYourCountry/src/reformyourcountry/controller/ActionListController.java
+++ trunk/ReformYourCountry/src/reformyourcountry/controller/ActionListController.java
@@ -1,13 +1,11 @@
 package reformyourcountry.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 
 import reformyourcountry.model.Action;
 import reformyourcountry.repository.ActionRepository;
@@ -19,7 +17,6 @@ public class ActionListController {
   
     @RequestMapping("/actionlist")
     public ModelAndView actionListDisplay(){
-        
         List<Action> actionList = actionRepository.findAll();
         return new ModelAndView("actionlistdisplay","actions",actionList);
     }
