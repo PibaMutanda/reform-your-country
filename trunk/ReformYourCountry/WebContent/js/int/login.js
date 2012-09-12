4cd2487edc264975e435ed8c815d1d1967c36eb7
lionel.timmerman@gmail.com <lionel.timmerman@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-12 10:05:35 +0000

    login dialog

---
+++ trunk/ReformYourCountry/WebContent/js/int/login.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+			   // do stuff when DOM is ready
+			
+	       // initialise jquery ui dialog box
+			$('#logindialog').dialog({
+				title :   "Identification",
+				autoOpen: false,
+				width: 300, 
+				buttons: {
+					"Se connecter": function() {
+										
+						var username  = $(this).children().children('input[name="identifier"]').attr('value');
+						var password =  $(this).children().children('input[name="password"]').attr('value');
+						var checkbox = $(this).children().children('input[name="keepLoggedIn"]').is(':checked');
+						
+						console.log(username);
+						console.log(password);
+						console.log(checkbox);
+						
+					// execute the ajax request
+						var request = $.ajax({
+			    	    	  url: "ajax/loginsubmit",
+			    	    	  type: "POST",
+			    	    	  data: { identifier : username,
+			    	    		      password : password,
+			    	    		      keepLoggedIn : checkbox},
+			    	    	  dataType: "html"
+			    	    	});
+						// if success
+						request.done(function(data) {
+							console.log(data);
+							location.reload();
+							$('#logindialog').dialog("close");
+						});
+						
+						// if fail
+						request.fail(function(msg){
+							console.log(msg.responseText);
+							$('#logindialog').children('label[id="errorMsg"]').text(msg.responseText);
+						});
+						
+						
+					},
+					"Anuler": function() {
+						$('#logindialog').dialog("close");
+					}
+				},
+				
+				show:"clip",
+				hide:"clip"
+				
+			});
+			
+	    	
+	   
+	
+			   
+	    $("#login").click(function(){
+	    	         
+	    	        $('#logindialog').children('label[id="errorMsg"]').empty(); // reset the error message
+	  	        	$('#logindialog').dialog('open');
+	  		
+				
+				return false;
+			});
+	    
+		  
+	
+	 });
