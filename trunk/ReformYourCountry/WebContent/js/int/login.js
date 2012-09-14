8aebd7a55bb83e877e82e22a9f1ba475d6e9e475
lionel.timmerman@gmail.com <lionel.timmerman@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-14 08:20:38 +0000

    login dialog

--- trunk/ReformYourCountry/WebContent/js/int/login.js
+++ trunk/ReformYourCountry/WebContent/js/int/login.js
@@ -1,90 +1,99 @@
-$(document).ready(function() {
-			   // do stuff when DOM is ready
+$(document).ready(function() {			   // do stuff when DOM is ready
 			
-	       // initialise jquery ui dialog box
-			$('#logindialog').dialog({
-				title :   "Identification",
-				autoOpen: false,
-				width: 300, 
-				buttons: {
-					"Se connecter": function() {
-										
-						var username  = $(this).children().children('input[name="identifier"]').attr('value');
-						var password =  $(this).children().children('input[name="password"]').attr('value');
-						var checkbox = $(this).children().children('input[name="keepLoggedIn"]').is(':checked');
-						
-					//	console.log(username);
-					//	console.log(password);
-					//	console.log(checkbox);
-						
-					// execute the ajax request
-						var request = $.ajax({
-			    	    	  url: "ajax/loginsubmit",
-			    	    	  type: "POST",
-			    	    	  data: { identifier : username,
-			    	    		      password : password,
-			    	    		      keepLoggedIn : checkbox},
-			    	    	  dataType: "html"
-			    	    	});
-						// if success
-						request.done(function(data) {
-					//		console.log(data);
-							location.reload();
-							$('#logindialog').dialog("close");
-						});
-						
-						// if fail
-						request.fail(function(msg){
-					//		console.log(msg.responseText);
-							$('#logindialog').children('label[id="errorMsg"]').text(msg.responseText);
-						});
-						
-						
-					},
-					"Anuler": function() {
-						$('#logindialog').dialog("close");
-					}
-				},
-				
-				show:"clip",
-				hide:"clip"
-				
-			});
-			
-	    	
-	   
 	
-			   
-	    $("#login").click(function(){
-	    	         
-	    	        $('#logindialog').children('label[id="errorMsg"]').empty(); // reset the error message
-	  	        	$('#logindialog').dialog('open');
-	  		
-				
-				return false;
-			});
-	    
-	    
-	    
-	    $("#logout").click(function(){
-	         
-	   
+			
+	function sendConnectRequest(){
+
+
+		var username  = $("#logindialog").children().children('input[name="identifier"]').attr('value');
+		var password =  $("#logindialog").children().children('input[name="password"]').attr('value');
+		var checkbox = $("#logindialog").children().children('input[name="keepLoggedIn"]').is(':checked');
+
+
+		// execute the ajax request.
+		var request = $.ajax({
+			url: "ajax/loginsubmit",
+			type: "POST",
+			data: { identifier : username,
+				password : password,
+				keepLoggedIn : checkbox},
+				dataType: "html"
+		});
+
+		// if success
+		request.done(function(data) {
+
+			location.reload();
+			$('#logindialog').dialog("close");
+		});
+
+		// if fail
+		request.fail(function(msg){
+
+			$('#logindialog').children('label[id="errorMsg"]').text(msg.responseText);
+		});
+
+
+	}  
+
+	$("#login").click(function(){  // To open the dialog box when clicking on the upper right corner login link.
+		// initialise jquery ui dialog box. It is empty and does not open.
+		$('#logindialog').dialog({
+			title :   "Identification",
+			autoOpen: false,
+			width: 320, 
+			buttons: {
+				"Se connecter": function() {  // Called on button click;
+					sendConnectRequest();
+				},
+				"Anuler": function() {
+					$('#logindialog').dialog("close");
+				}
+			},
+			show:"clip",
+			hide:"clip"
+		});
 		
-			var request = $.ajax({
-  	    	  url: "ajax/logout",
-  	    	  type: "POST", 	
-  	    	  dataType: "html"
-  	    	});
-			// if success
-			request.done(function(data) {
-			//	console.log(data);
-				location.reload();
-				
-			});
-	    	
-	    	
-		
+		$('#logindialog').load("ajax/login", function(){
+			$('#logindialog').children('label[id="errorMsg"]').empty(); // reset the error message
+			$('#logindialog').dialog('open');
+		});
+
 		return false;
 	});
+
+	// KeyBoard shorcut : enter send the connection request
+	$("#logindialog").keydown(function(e){
+
+		if(e.which == 13) {
+
+			sendConnectRequest();
+
+		}
+	});
+
+
+	// TODO add secured path to avoid malformed url error due to missing parameter for spring controller
+	//maybe better solution ??
+
+
+	$("#logout").click(function(){
+		var request = $.ajax({
+			url: "ajax/logout",
+			type: "POST", 	
+			dataType: "text"
+		});
+		// if success
+		request.done(function(data) {
 	
-	 });
+			location.reload();
+
+		});
+
+
+
+		return false;
+	});
+
+
+});
