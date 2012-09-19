bb35632ae80dbec7fd706f3edfd42bc401502294
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-19 14:33:11 +0000

    a funny script who going to break your life if you execute it in prod server. <:0)

---
+++ trunk/ReformYourCountry/SQL/deleteAllData.sql
@@ -0,0 +1,17 @@
+﻿DELETE FROM voteargument ;
+DELETE FROM argument ;
+DELETE FROM article_action ;
+DELETE FROM article ;
+DELETE FROM comment ;
+DELETE FROM article_action ;
+DELETE FROM action ;
+
+DELETE FROM book ;
+
+DELETE FROM groupreg ;
+DELETE FROM groups ;
+DELETE FROM mail ;
+DELETE FROM users_privileges ;
+DELETE FROM users ;
+DELETE FROM voteaction ;
+ 
