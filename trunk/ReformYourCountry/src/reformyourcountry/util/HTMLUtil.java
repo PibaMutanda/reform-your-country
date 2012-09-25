43be243bb304daa78e691348724478021ce23009
jeromepanaioli@gmail.com <jeromepanaioli@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-25 14:11:30 +0000

    article dsp

--- trunk/ReformYourCountry/src/reformyourcountry/util/HTMLUtil.java
+++ trunk/ReformYourCountry/src/reformyourcountry/util/HTMLUtil.java
@@ -4,37 +4,6 @@ import reformyourcountry.model.Book;
 
 public class HTMLUtil {
     
-    public static String getBookFragment(Book book, boolean inToolTip) {
-        
-        String block = "";
 
-        if (inToolTip) {
-            block += "<div id ='book-"+book.getAbrev()+"' style='display:none;'>"; // This div will be "removed" by jQuery bubbles (tooltip)
-        }
-        block += "<div class='book'>";  // Will be in the tooltip too.
-        
-        
-        if(inToolTip)
-            block += "<a href='booklist#"+book.getAbrev()+"'>"; 
-        else
-            block += "<a id ='"+book.getAbrev()+"'>";
-        block += book.getTitle()+"</a><br/>";
-        block += book.getAuthor()+"<br/> ";
-        block += book.getPubYear()+"<br/> ";
-        if(book.isHasImage())
-            block += "<img src='gen" + FileUtil.BOOK_SUB_FOLDER + FileUtil.BOOK_RESIZED_SUB_FOLDER +"/"+book.getId()+".jpg' alt='"+book.getTitle()+"' class='imgbook'>";
-
-        block += "<p>"+book.getDescription()+"</p>"+
-                "<a href = '"+book.getExternalUrl()+"' target = '_blank'\">Lien externe</a>";
-        
-        block +="</div>\n";
-        
-        if (inToolTip) {
-            block +="</div>\n";
-        }
-
-        return block;
-
-    }
 
 }
