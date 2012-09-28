8bf9c7102cbf70747e1e34781b1611889ed067ca
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-09-28 14:52:58 +0000

    remove relaese date field

--- ReformYourCountry/src/reformyourcountry/repository/ArticleRepository.java
+++ ReformYourCountry/src/reformyourcountry/repository/ArticleRepository.java
@@ -10,14 +10,19 @@ import reformyourcountry.model.Article;
 public class ArticleRepository extends BaseRepository<Article>{
     
     public List<Article> findAll(){
-        return    em.createQuery("select a from Article a order by a.releaseDate").getResultList();
+        return    em.createQuery("select a from Article a order by a.publishDate").getResultList();
     }
     
     public List<Article> findAllWithoutParent(){
-        return    em.createQuery("select a from Article a where a.parent is null order by a.releaseDate").getResultList();
+        return    em.createQuery("select a from Article a where a.parent is null order by a.publishDate").getResultList();
     }
     
     public Article findByShortName(String shortName){
     	return getSingleOrNullResult(em.createQuery("select a from Article a where a.shortName = :shortname").setParameter("shortname",shortName));
     }
+    
+    public Article findByTitle(String title){
+        return getSingleOrNullResult(em.createQuery("select a from Article a where a.title = :title").setParameter("title",title));
+    }
+    
 }
