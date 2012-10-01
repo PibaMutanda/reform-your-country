21524442afa86c713620745d193f361cfaea0f52
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-10-01 10:26:20 +0000

    some refactoring

--- ReformYourCountry/WebContent/js/int/autosave.js
+++ ReformYourCountry/WebContent/js/int/autosave.js
@@ -3,96 +3,49 @@ $(document).ready(function() {
 	//disable the save button
 	$("#save").attr('disabled', 'disabled');
 
-	var timer = null;  // Current timer waiting to save the content in a few seconds.
-
-	$("#articleContent,#title,#datepicker,#publicView1").keydown(function(event){
+	$(".autosaveable").keydown(function(event){
+		//it save when we type CTRL+S
 		if(event.ctrlKey  && event.keyCode == 83) { // touche s = 83
-			console.log(event.wich + " "+ event.keyCode);	
 			event.preventDefault();
-			performChange(0);  // 0 = immediately			
+			performChange();
 		}
 	});
 
-	$("#articleContent,#title,#datepicker,#publicView1").keyup(function(event) {
-		performChange(5000);  // We'll save in 120 seconds from now.
-	});
-
-	$("#datepicker,#publicView1").focusout(function(){
-		performChange(0);  // When the field loses focus, we save immediately.
-	});
-
-	
-	// Function called when a changed is performed on the article.
-	// ms = number of miliseconds to wait before saving.
-	function performChange(ms) {
+	$(".autosaveable").keyup(function(event) {
 		$("#save").removeAttr('disabled');  // Make the save button enabled (the text effectively changed since the last save).
-	
-        if (timer == null) { // No timer is currently on-going => we need to create one.
-		
-   		  timer =  // We add an element to the array.  
-				setTimeout(function(){
-				////// We effectively start saving from here
-				$("#saving").text("savegarde en cours....");		
-				
-				/*	console.log($("#title").attr("value"));
-  		            console.log($("#datepicker").attr("value"));
-  		            console.log($("#publicView1").is(":checked"));
-  		             console.log($("#content").val());
-  		             console.log($("#id").attr("value"));*/
 
-				var title = $("#title").attr("value");
-				var date = $("#datepicker").attr("value");
-				var publicview = $("#publicView1").is(":checked") ;
-				var publicviewForSpring;
-
-				if($("#publicView1").is(":checked")){
-					publicviewForSpring = "on";
-
-				} else {
-					publicviewForSpring = "off";
-					
-				}
-				var content = $("#articleContent").val();
-				var id = $("#id").attr("value");
-
-				// Ask the server to save
-				var request = $.ajax({
-					url: "ajax/articleeditsubmit",
-					type: "POST",
-					data: { title : title,
-						publishDate : date,
-						publicView:publicview,
-						_publicView : publicviewForSpring,
-						content : content,
-						id : id},
-						dataType: "html"
-				});
-
-				request.done(function(){
-					$("#save").attr('disabled', 'disabled');
-					$("#saving").text("sauvé à "+getDate());	
-					timer = null;
-				});
-
-
-			},ms);
-
+		if(event.ctrlKey == false  &&  event.keyCode != 83) {// get the save timer only when new content not after a CTRL+S pressed
+			setTimeout(function(){performChange();}, 120000);  // We'll save in 120 seconds from now.
 		}
+	});
 
+
+	// Function called when a changed is performed on the article.
+	function performChange() {
+		////// We effectively start saving from here
+		$("#saving").text("sauvegarde en cours....");		
+		//get the action value from the form and add ajax/ before because this is a project convention for ajax request
+		$.post("ajax/"+$(".autosaveable").closest("form").attr("action"), 
+				$(".autosaveable").closest("form").serialize())//closest get the closest ancestor or itself . 
+																						//For example if the tag with autosaveable  is contain in a form(with id #exmaple1) and this form is contain in an other form(with id #exmaple2) 
+																						//only #exmaple1 is selected and submit via .serialize().
+		.done(function(){
+			$("#save").attr('disabled', 'disabled');
+			$("#saving").text("sauvé à "+getDate());	
+		});
 	}
-
 
 	function getDate(){
 		var now = new Date();
 		var hours = now.getHours();
 		var minutes = now.getMinutes();
-		var ms = now.getMilliseconds();
+		var secondes = now.getSeconds();
 
 		if (hours < 10) hours = "0"+hours;
 		if (minutes < 10) minutes ="0"+minutes;
+		if (secondes < 10) secondes ="0"+secondes;
 
-		return hours+":"+minutes+":"+ms;
+		return hours+":"+minutes+":"+secondes;
 	}
-
 
 });
