752edf72016c3b790496f00a1cc8747e30a41533
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-10-05 14:51:38 +0000

    some minor but not all what i wanted

--- ReformYourCountry/WebContent/js/int/help.js
+++ ReformYourCountry/WebContent/js/int/help.js
@@ -1,18 +1,20 @@
 $(function(){  // on document ready
-	///// Position the "?" help handle, and the div for the help text.
-	var bodyTemplate = $('.body-template');
-	var rightEdge = bodyTemplate.offset().left + bodyTemplate.width() + 20;
-	$('#helphandle').css('left',  rightEdge);  // clickable "?"
 	
-	var leftPosOfHelpText = rightEdge-$('#helptext').width();
-	$('#helptext').css('left',  leftPosOfHelpText);  // large div with the help text.
+	$('#helphandle').show();	
+	$('#helptext').hide();
+	
+	///// Position the "?" help handle, and the div for the help text.
+	var posLeft = $('.main-holder').position().left+$('.main-area').width();
+	
+	$('#helphandle').css('left',  posLeft);  // clickable "?"
+	$('#helphandle').css('top',  $('.main-holder').offset().top);  // clickable "?"
+	
+	$('#helptext').css('left',  posLeft-$('#helptext').width());  // large div with the help text.
 	
     /// Computes the height of the help text to go to the bottom border of the browser.
-	var helpTextTop = $('#helptext').offset().top - $(window).scrollTop();  // Top of the help div, relative to the window.
-	$('#helptext').css('height', $(window).height() - helpTextTop);
+	// Top of the help div, relative to the window.
+	$('#helptext').css('height', $(window).height() - $('#helptext').offset().top - $(window).scrollTop());
 	
-	////// Hide the help text and show on click.
-	$('#helptext').hide();
 	$("#click").click(function(){
 	   $('#helptext').load("helptxt/edithelp.html");  // takes the content from the server.
     	$("#helptext").slideToggle("slow");//replace functions "slideUp" and "slideDown" in a single function "slideToggle"
