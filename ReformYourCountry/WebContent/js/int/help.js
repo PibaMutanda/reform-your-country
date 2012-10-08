dc063336e7ea024bbd7e5d75296fbd710102f198
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-10-08 14:25:21 +0000

    fix helptext position

--- ReformYourCountry/WebContent/js/int/help.js
+++ ReformYourCountry/WebContent/js/int/help.js
@@ -1,6 +1,5 @@
 $(function(){  // on document ready
-	
-	$('#helphandle').show();	
+
 	$('#helptext').hide();
 	
 	///// Position the "?" help handle, and the div for the help text.
@@ -8,15 +7,18 @@ $(function(){  // on document ready
 	
 	$('#helphandle').css('left',  posLeft);  // clickable "?"
 	$('#helphandle').css('top',  $('.main-holder').offset().top);  // clickable "?"
-	
-	$('#helptext').css('left',  posLeft-$('#helptext').width());  // large div with the help text.
-	
-    /// Computes the height of the help text to go to the bottom border of the browser.
-	// Top of the help div, relative to the window.
-	$('#helptext').css('height', $(window).height() - $('#helptext').offset().top - $(window).scrollTop());
+
+	$('#helphandle').show();
 	
 	$("#click").click(function(){
-	   $('#helptext').load("helptxt/edithelp.html");  // takes the content from the server.
-    	$("#helptext").slideToggle("slow");//replace functions "slideUp" and "slideDown" in a single function "slideToggle"
+		
+		$('#helptext').css('left',  posLeft-$('#helptext').width());  // large div with the help text.
+
+		/// Computes the height of the help text to go to the bottom border of the browser.
+		// Top of the help div, relative to the window.
+		$('#helptext').css('height', $(window).height() - $('#helptext').offset().top - $(window).scrollTop());
+		
+		$('#helptext').load("helptxt/edithelp.html");  // takes the content from the server.
+		$("#helptext").slideToggle("slow");//replace functions "slideUp" and "slideDown" in a single function "slideToggle"
     });	
 });
