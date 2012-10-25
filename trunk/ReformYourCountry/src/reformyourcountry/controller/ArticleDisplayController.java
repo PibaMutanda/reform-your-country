355f5c570c3c21d52ae2e8616ff493550ff35925
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-10-25 08:46:24 +0000

    video carousel

--- trunk/ReformYourCountry/src/reformyourcountry/controller/ArticleDisplayController.java
+++ trunk/ReformYourCountry/src/reformyourcountry/controller/ArticleDisplayController.java
@@ -1,9 +1,6 @@
 package reformyourcountry.controller;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collections;
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +13,7 @@ import reformyourcountry.converter.BBConverter;
 import reformyourcountry.model.Article;
 import reformyourcountry.repository.ArticleRepository;
 import reformyourcountry.repository.BookRepository;
+import reformyourcountry.repository.VideoRepository;
 import reformyourcountry.security.Privilege;
 import reformyourcountry.security.SecurityContext;
 import reformyourcountry.util.DateUtil;
@@ -29,6 +27,7 @@ public class ArticleDisplayController extends BaseController<Article> {
     
     @Autowired BookRepository bookRepository;
     @Autowired ArticleRepository articleRepository;
+    @Autowired VideoRepository videoRepository;
     
 	@RequestMapping(value={"/{articleUrl}"})
 	public ModelAndView displayArticle( @PathVariable("articleUrl") String articleUrl){
@@ -65,7 +64,7 @@ public class ArticleDisplayController extends BaseController<Article> {
         BBConverter bbc = new BBConverter(bookRepository, articleRepository);
         mv.addObject("articleContent", bbc.transformBBCodeToHtmlCode(article.getLastVersion().getContent()));
         mv.addObject("articleSummary", bbc.transformBBCodeToHtmlCode(article.getLastVersion().getSummary()));
-        
+        mv.addObject("videoList",videoRepository.findAllVideoForAnArticle(article));
         return mv;
 	}
 
--- trunk/ReformYourCountry/src/reformyourcountry/repository/VideoRepository.java
+++ trunk/ReformYourCountry/src/reformyourcountry/repository/VideoRepository.java
@@ -1,12 +1,27 @@
 package reformyourcountry.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
+import reformyourcountry.model.Article;
 import reformyourcountry.model.Video;
 
 
 @Repository
+@SuppressWarnings("unchecked")
 public class VideoRepository extends BaseRepository<Video> {
+
+    public List<Video> findAllVideoForAnArticle(Article article){
+        return findAllByArticle(article.getId());
+    }
+    
+    
+    public List<Video> findAllByArticle(Long articleId) {
+        return em.createQuery("select v from Video v where v.article.id =:id order by v.createdOn DESC")
+                .setParameter("id", articleId)
+                .getResultList();
+    }
 
 	
 	
