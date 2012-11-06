8a40123e9c051c9bc527ac0f6861623a711f2efe
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-11-06 10:23:47 +0000

    entities goodexample and votegoodexample

--- trunk/ReformYourCountry/src/reformyourcountry/model/GoodExample.java
+++ trunk/ReformYourCountry/src/reformyourcountry/model/GoodExample.java
@@ -19,7 +19,13 @@ public class GoodExample extends BaseEntity {
 	@Column(length = 100, unique = true, nullable = false)// need nullable= false for schemaupdate
 	private String title;
 	
-	@Lob
+    @Lob
+    /*Forcing type definition to have text type column in postgresql instead of automatic indirect storage of large object (postgresql store lob in a separate table named pg_largeobject and store his id in the "content" column).
+     *Without forcing, JDBC driver use write() method of the BlobOutputStream to store Clob into the database;
+     * this method take an int as parameter an convert it into a byte causing lose of 3 byte information so character are render as ASCII instead of UTF-8 expected .
+     * @see http://stackoverflow.com/questions/9993701/cannot-store-euro-sign-into-lob-string-property-with-hibernate-postgresql
+     * @see http://stackoverflow.com/questions/5043992/postgres-utf-8-clobs-with-jdbc
+     */
     @Type(type="org.hibernate.type.StringClobType")
 	private String description;
 	
@@ -30,6 +36,8 @@ public class GoodExample extends BaseEntity {
 	
 	@ManyToMany(mappedBy = "goodExamples")
 	private List<Article> articles = new ArrayList <Article>();
+	
+	private int voteCount;
 
 	public String getTitle() {
 		return title;
@@ -58,6 +66,14 @@ public class GoodExample extends BaseEntity {
 	public List<Article> getArticles() {
 		return articles;
 	}
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
 	
 	
 }
---
+++ trunk/ReformYourCountry/src/reformyourcountry/model/VoteGoodExample.java
@@ -0,0 +1,33 @@
+package reformyourcountry.model;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class VoteGoodExample extends BaseEntity{
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private GoodExample goodExample;
+	@ManyToOne
+    @JoinColumn(nullable = false)
+	private User user;
+	
+    public GoodExample getGoodExample() {
+        return goodExample;
+    }
+
+    public void setGoodExample(GoodExample goodExample) {
+        this.goodExample = goodExample;
+    }
+
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
