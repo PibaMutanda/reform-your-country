ca6cc88087974db42b42144d14a9087b04b46276
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-11-09 10:41:27 +0000

    little fix

--- ReformYourCountry/SQL/0.0.3.sql
+++ ReformYourCountry/SQL/0.0.3.sql
@@ -1,16 +1,4 @@
--- 2012-10-16 Thomas VR
-
-    alter table article add column content text;
-
-    alter table article add column summary text;
-
-    alter table article add column toclassify text;
-  
-           
 -- 2012-10-12 maxime
-    ALTER TABLE article DROP COLUMN content ;
-    ALTER TABLE article DROP COLUMN toclassify ;
-    ALTER TABLE article DROP COLUMN summary ;
     
     alter table article 
         add column lastversion_id int8;
@@ -50,12 +38,10 @@
 
     alter table article 
         add column toclassify text;
--- 2012-10-12 Maxime
+
    ALTER TABLE article DROP COLUMN content;
    ALTER TABLE article DROP COLUMN toclassify ;
    ALTER TABLE article DROP COLUMN summary ;
-        
-        
      
 --2012-10-16
     alter table book add column subtitle varchar(255);
