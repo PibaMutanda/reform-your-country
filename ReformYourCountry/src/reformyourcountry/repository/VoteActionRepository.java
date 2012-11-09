9162b483924af500028aa3f057ea05a4be6fa43e
Thom.VanRoy@gmail.com <Thom.VanRoy@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-11-09 13:05:34 +0000

    vote Graph WIP

--- ReformYourCountry/src/reformyourcountry/repository/VoteActionRepository.java
+++ ReformYourCountry/src/reformyourcountry/repository/VoteActionRepository.java
@@ -20,4 +20,9 @@ public class VoteActionRepository extends BaseRepository<VoteAction> {
         return (Long)em.createQuery("select sum(v.value) from VoteAction v where v.action.id = :idAction")
                 .setParameter("idAction", idAction).getSingleResult();
     }
+    public Long getNumberOfVotesByValue(Long idAction, int voteValue){
+    	Long result = (Long)em.createQuery("select count(v) from VoteAction v where v.action.id = :idAction and v.value = :voteValue")
+    			.setParameter("idAction", idAction).setParameter("voteValue", voteValue).getSingleResult();
+    	return result!=null ? result : 0l;
+    }
 }
