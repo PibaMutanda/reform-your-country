4445bd28cdf2f6972cf54b50a58654c3026b3ba6
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-11-09 10:46:56 +0000

    little fix

--- trunk/ReformYourCountry/SQL/0.0.2.sql
+++ trunk/ReformYourCountry/SQL/0.0.2.sql
@@ -1,5 +1,3 @@
--- 2012-09-17 Lionel (pour Jamal)
-    alter table groups add column hasimage boolean default '0' not null; 
 -- 2012-09-18 Jamal
     alter table users add column url varchar(255);
 -- 2012-09-21
