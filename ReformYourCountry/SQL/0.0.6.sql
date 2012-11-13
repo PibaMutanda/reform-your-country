241e38cbf78aa7d4ac80325258883a42ebd2470d
Mr.Maxime.Sauvage@gmail.com <Mr.Maxime.Sauvage@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-11-13 16:10:11 +0000

    delete obsolete jointable

---
+++ ReformYourCountry/SQL/0.0.6.sql
@@ -0,0 +1,33 @@
+-- 2012-11-13 maxime
+    DROP TABLE article_goodexample ;
+    create table article_goodexample (
+        articles_id int8 not null,
+        goodexamples_id int8 not null
+    );
+
+    alter table article_goodexample 
+        add constraint fkfa5423e433d25d2c 
+        foreign key (articles_id) 
+        references article;
+
+    alter table article_goodexample 
+        add constraint fkfa5423e4bd17c73a 
+        foreign key (goodexamples_id) 
+        references goodexample;
+    
+    DROP TABLE article_action  ;
+    create table article_action (
+        articles_id int8 not null,
+        actions_id int8 not null
+    );
+
+    alter table article_action 
+        add constraint fk351605dff1d594da 
+        foreign key (actions_id) 
+        references action;
+
+    alter table article_action 
+        add constraint fk351605df33d25d2c 
+        foreign key (articles_id) 
+        references article;
+    
