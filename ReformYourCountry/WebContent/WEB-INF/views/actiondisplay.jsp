63f8be979345312c4fba6d24064e5ec8e02dbdf0
Thom.VanRoy@gmail.com <Thom.VanRoy@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-11-14 15:33:03 +0000

    votes actions

--- ReformYourCountry/WebContent/WEB-INF/views/actiondisplay.jsp
+++ ReformYourCountry/WebContent/WEB-INF/views/actiondisplay.jsp
@@ -6,12 +6,14 @@
 <head>
 <meta name="description" lang="fr" content="${action.shortDescription}"/>
 <meta name="robots" content="index, follow"/>
+<script type="text/javascript" src="js/int/voteaction.js"></script>
 <script type="text/javascript" src="js/ext/d3.v2.min.js"></script>
 </head>
 <body>
     <ryctag:pageheadertitle title="${action.title}"/>
 
-		<div style=" width: 100%;">
+    <%-- Action text --%>
+	<div style=" width: 100%;">
 			<div style="font-size:1.3em;">
 				${action.content}
 			</div>
@@ -31,20 +33,19 @@
 					
 				</div>
 			</div>
-		</div>
+	</div>
 		
-
+   <%-- Chart --%>
 	<div id="voteGraph" style=" width: 500px; margin-left: 150px;background:url(/images/_global/separator3.gif) 0 0 repeat-x ;padding-top:10px;">
 	</div>
 	<div id="voteContainer" > <%-- Will be re-filled through Ajax too --%>
-		
-	  <%@include file="voteaction.jsp"%>
+	    <%@include file="voteaction.jsp"%>
 	</div>
+	
+	<%-- Arguments --%>
 	<div id="argContainer"  style="background:url(/images/_global/separator3.gif) 0 0 repeat-x ;padding-top:10px;"> <%-- Will be re-filled through Ajax --%>
 		<%@include file="argument.jsp" %>
 	</div>
-<script type="text/javascript">
-var chart = d3.select("#voteGraph").append("svg").attr("width", "500").attr("height", "130");
-</script>
+	
 </body>
 </html> 
--- ReformYourCountry/src/reformyourcountry/controller/ActionDisplayController.java
+++ ReformYourCountry/src/reformyourcountry/controller/ActionDisplayController.java
@@ -1,38 +1,64 @@
 package reformyourcountry.controller;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import reformyourcountry.model.Action;
+import reformyourcountry.model.VoteAction;
+import reformyourcountry.repository.ActionRepository;
 import reformyourcountry.repository.ArgumentRepository;
+import reformyourcountry.repository.UserRepository;
 import reformyourcountry.repository.VoteActionRepository;
+import reformyourcountry.security.SecurityContext;
 import reformyourcountry.service.ActionService;
-import reformyourcountry.service.ArgumentService;
 
 @Controller
-@RequestMapping("/action")
 public class ActionDisplayController extends BaseController<Action> {
     
     
     @Autowired VoteActionRepository voteActionRepository;
     @Autowired ArgumentRepository argumentRepository;
     @Autowired ActionService actionService;
+    @Autowired ActionRepository actionRepository;
+    @Autowired UserRepository userRepository;
 
-    @RequestMapping("/{actionUrl}")
+    @RequestMapping("/action/{actionUrl}")
     public ModelAndView displayAction(@PathVariable("actionUrl") String actionUrl) {
         Action action = getRequiredEntityByUrl(actionUrl);
         ModelAndView mv = actionService.getActionModelAndView(action,"actiondisplay");
-        
         
         actionService.putGraphNumbersInModelAndView(mv,action);
         
         return  mv;
     }
+    @RequestMapping("ajax/vote")
+    public ModelAndView vote(@RequestParam("actionId") long actionId, @RequestParam("vote") int vote){
+        SecurityContext.assertUserIsLoggedIn();
+        VoteAction va = voteActionRepository.findVoteActionForUser(SecurityContext.getUser(), actionId);
+        if (va==null){
+            va = new VoteAction(vote, actionRepository.find(actionId), SecurityContext.getUser());
+            voteActionRepository.persist(va);
+        }else{
+            va.setValue(vote);
+            voteActionRepository.merge(va);
+        }
+        
+        ModelAndView mv = new ModelAndView("voteaction");
+        
+        Action action = getRequiredEntity(actionId);
+        
+        actionService.putGraphNumbersInModelAndView(mv,action);
+        
+        mv.addObject("action", action);
+        mv.addObject("resultVote", voteActionRepository.getTotalVoteValue(actionId));
+        if (va != null){ 
+            mv.addObject("vote",va);
+        }
+        return mv;
+    }   
 }
