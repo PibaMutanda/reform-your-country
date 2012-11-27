dcab9823a89332923692b3e920da61e1e8b2052e
lafailledelphine@gmail.com <lafailledelphine@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-11-27 14:09:39 +0000

    goodexample by createdOn

--- trunk/ReformYourCountry/SQL/0.0.7.sql
+++ trunk/ReformYourCountry/SQL/0.0.7.sql
@@ -5,3 +5,10 @@ ALTER TABLE goodexample DROP COLUMN  url ;
 
     alter table goodexample 
         add column publishdate timestamp; 
+        
+-- 2012-11-27 Delphine
+
+    alter table goodexample 
+        drop column publishdate timestamp; 
+        
+        
