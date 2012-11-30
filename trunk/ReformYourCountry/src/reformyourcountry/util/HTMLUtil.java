2202e3d0f5850f4d3eef995e8d6573b0b16fb9db
lionel.timmerman@gmail.com <lionel.timmerman@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-11-30 13:08:44 +0000

    replace antysamy by jsoup

--- trunk/ReformYourCountry/src/reformyourcountry/util/HTMLUtil.java
+++ trunk/ReformYourCountry/src/reformyourcountry/util/HTMLUtil.java
@@ -2,17 +2,17 @@ package reformyourcountry.util;
 
 import java.util.ArrayList;
 
-import org.owasp.validator.html.AntiSamy;
-import org.owasp.validator.html.CleanResults;
-import org.owasp.validator.html.Policy;
-import org.owasp.validator.html.PolicyException;
-import org.owasp.validator.html.ScanException;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+import reformyourcountry.model.User;
 
 public class HTMLUtil {
 	
-	private static String ANTISAMY_XML_FILE_LOCATION = Thread.currentThread().getContextClassLoader().getResource("antisamy-tinymce-1.4.4.xml").getFile();
-	private static Policy antiSamyPolicyInstance = null;
-	private static AntiSamy antiSamyInstance = null;
+
+	private static Whitelist whiteList = Whitelist.basic();
+	
+
     
 	/**
      * Remove as much HTML as possible.
@@ -22,51 +22,23 @@ public class HTMLUtil {
         return result;
     }
     
-    public static String cleanHtml(String htmlToCLean) {
-		String result = "if you see this something goes wrong with cleanHtml method";
-
-		try {
-			Policy policy = Policy.getInstance(ANTISAMY_XML_FILE_LOCATION);
-			AntiSamy as = new AntiSamy(); // Create AntiSamy object
-			CleanResults cr = as.scan(htmlToCLean, policy, AntiSamy.SAX);// Scan dirtyInput
-			result = cr.getCleanHTML();
-
-		} catch (PolicyException | ScanException e) {
-			throw new RuntimeException(e);
-		}
-
-		return result;
-    }
+ 
 
 	public static boolean isHtmlSecure(String htmlToCheck) {
     	ArrayList<?> error = null;
     	//sometimes controller send null parameter or empty so it isn't unsecure...
     	if ( htmlToCheck == null || htmlToCheck.isEmpty() ) {
     		return true;
-    	} else {
-    		try {
-    			error = getAntiSamyInstance().scan(htmlToCheck, antiSamyPolicyInstance, AntiSamy.SAX).getErrorMessages();
-    			System.out.println(error.toString());
-    		} catch (ScanException | PolicyException e) {
-    			return false;
-    		}
-    	}
-    	
-		return error.isEmpty();
+    	} 
+    	return Jsoup.isValid(htmlToCheck, whiteList);
     	
     }
 
-    private synchronized static AntiSamy getAntiSamyInstance() {
-    	if(antiSamyInstance == null){
-    		try {
-    			if(antiSamyPolicyInstance == null){
-    				antiSamyPolicyInstance = Policy.getInstance(ANTISAMY_XML_FILE_LOCATION);
-    			} 
-    		} catch (PolicyException e) {
-    			throw new RuntimeException("cannot get antiSamy policy instance");
-    		}
-    		antiSamyInstance = new AntiSamy(); // Create AntiSamy object
-    	}
-    	return antiSamyInstance;
-    }
+
+	
+	
+	public static String getUserPageLink(User user){
+	    
+	    return "<a href='/user/"+user.getUserName()+"'>"+user.getFullName()+"</a>";
+	}
 }
