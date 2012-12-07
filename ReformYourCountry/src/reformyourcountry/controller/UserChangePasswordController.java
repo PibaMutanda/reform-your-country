804b71cf98dca5eaf760fc2e5134f34d18b82f93
julienciarma@gmail.com <julienciarma@gmail.com@0814918a-3bd9-5067-5d2a-e9c44db9c170>
2012-12-07 14:11:14 +0000

    password

--- ReformYourCountry/src/reformyourcountry/controller/UserChangePasswordController.java
+++ ReformYourCountry/src/reformyourcountry/controller/UserChangePasswordController.java
@@ -1,5 +1,8 @@
 package reformyourcountry.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
 import reformyourcountry.model.User;
 import reformyourcountry.repository.UserRepository;
+import reformyourcountry.security.Privilege;
 import reformyourcountry.security.SecurityContext;
-import reformyourcountry.util.NotificationUtil;
 import reformyourcountry.util.SecurityUtils;
 
 @Controller
@@ -25,59 +27,76 @@ public class UserChangePasswordController {
 
 	@RequestMapping("/changepassword")
 	public ModelAndView userChangePassword(@ModelAttribute User user) {
-		
+
 		ModelAndView mv= new ModelAndView("userchangepassword");
 		mv.addObject("user",user);
+		mv.addObject("verifyOldPassword", needToVerifyOldPassword(user));
 		return mv;
 	}
 
 	@RequestMapping("/changepasswordsubmit")
 	public ModelAndView userChangePasswordSubmit(@Valid @ModelAttribute User user, BindingResult result,
-			@RequestParam("oldPassword") String oldPassword,
+			@RequestParam(value= "oldPassword",  required=false) String oldPassword,
 			@RequestParam("newPassword") String newPassword,
 			@RequestParam("confirmPassword") String confirmPassword) {
 
 		SecurityContext.assertCurrentUserMayEditThisUser(user);
-		
-		String errorNoOld=null;
-		String errorEmpty=null;
-		String errorDiff=null;
-		String htmlMessage="Votre nouveau mot de passe est bien enregistré";
+
 		if (result.hasErrors()) {
 			return new ModelAndView("redirect:user","id",user.getId());
 		}
 		else {
-			if(((!(SecurityUtils.md5Encode(oldPassword)).equals(user.getPassword()))||newPassword.equals("") || confirmPassword.equals("")||!confirmPassword.equals(newPassword))){
-				ModelAndView mv = new ModelAndView("userchangepassword");
+			Map<String, String> errorMsgMap = new HashMap<String, String>();
+
+
+
+			if(needToVerifyOldPassword(user)){
 				if (!(SecurityUtils.md5Encode(oldPassword)).equals(user.getPassword())) {
-					errorNoOld= "Vous n'avez pas bien encodé votre ancien mot de passe";
-				}else if (newPassword.equals("") || confirmPassword.equals("")) {
-					errorEmpty= "Veuillez encoder un nouveau mot de passe et/ou la confirmation de ce mot de passe";
-				}else if (!confirmPassword.equals(newPassword)) {
-					errorDiff= "La confirmation du mot de passe et le nouveau mot de passe ne correspondent pas";
+					errorMsgMap.put("errorNoOld", "Vous n'avez pas bien encodé votre ancien mot de passe");
 				}
-				mv.addObject("user",user);
-				mv.addObject("errorNoOld",errorNoOld);
-				mv.addObject("errorEmpty",errorEmpty);
-				mv.addObject("errorDiff",errorDiff);
-				return mv;
 			}
-			user.setPassword(SecurityUtils.md5Encode(confirmPassword));
-			user.setPasswordKnownByTheUser(true);
-			userRepository.merge(user);
-			NotificationUtil.addNotificationMessage(htmlMessage);
-			return new ModelAndView("redirect:/user/"+user.getUserName());	
+
+			if (newPassword.equals("") || confirmPassword.equals("")) {
+				errorMsgMap.put("errorEmpty", "Veuillez encoder un nouveau mot de passe et/ou la confirmation de ce mot de passe");
+			} else if (!confirmPassword.equals(newPassword)) {
+				errorMsgMap.put("errorDiff", "La confirmation du mot de passe et le mot de passe ne correspondent pas");
+			}
+
+
+			if (!errorMsgMap.isEmpty()) {
+				ModelAndView errorDspMv = new ModelAndView("userchangepassword");
+				for (String msgKey : errorMsgMap.keySet()) {
+					errorDspMv.addObject(msgKey, errorMsgMap.get(msgKey));	
+				}
+				errorDspMv.addObject("user", user);
+				return errorDspMv;
+			} else {
+				user.setPassword(SecurityUtils.md5Encode(confirmPassword));
+				user.setPasswordKnownByTheUser(true);
+				userRepository.merge(user);
+				NotificationUtil.addNotificationMessage("Votre nouveau mot de passe est bien enregistré");
+				return new ModelAndView("redirect:/user/"+user.getUserName());
+			}				
 
 		}
-		
-    
+
 	}
 
+	
+	private boolean needToVerifyOldPassword(User user) {
+		// If admin and edits somebody else => we don't ask for the old password.
+		if(SecurityContext.isUserHasPrivilege(Privilege.MANAGE_USERS) && !user.equals(SecurityContext.getUser())){
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
 
 	@ModelAttribute
 	public User findUser(@RequestParam("id") Long id){
 		return  userRepository.find(id);
 	}
-	
-   
+
+
 }
